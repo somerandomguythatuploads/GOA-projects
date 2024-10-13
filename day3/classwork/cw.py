2da50ee17ec2c73b1c9adc94c985dd98ce60ebe7
somerandomguythatuploads <sanikidzegiorgi26@gmail.com>
2024-10-13 14:37:16 +0400

    commit

---
+++ day3/classwork/cw.py
@@ -0,0 +1,10 @@
+name = 'gio'
+last_name = 'sanikidze'
+country = 'republic of Georgia'
+hobby = 'astronomy'
+car = 'audi rs7'
+place_where_i_live = 'tbilisi'
+
+
+
+print('hello my name is' + name + 'and my last name is' "last_name" + 'i am from' + country + 'and i live in the city of' + place_where_i_live)
