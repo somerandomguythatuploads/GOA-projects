cd218ba9ef2b59425e858fcf08aa6a654f8cb687
somerandomguythatuploads <sanikidzegiorgi26@gmail.com>
2024-10-20 14:33:41 +0400

    commit

--- python/day4/classwork/cw.py
+++ python/day4/classwork/cw.py
@@ -1,1 +1,42 @@
-#
+#task 1(2):
+#--------------------------------------------------------------------
+
+
+
+a = 1
+b = 4
+
+c = 5
+d = 20
+
+B = 100
+A = 2
+
+
+print(a + b)
+print(c + d)
+print(A + B)
+
+
+print(b - a)
+print(d - c)
+print(B - A)
+
+
+print(b / 1)
+print(d / 5)
+print(100 / 2)
+
+
+print(a * b)
+print(c * d)
+print(A * B)
+
+
+
+#task 2(1):
+#--------------------------------------------------------------------
+
+
+message = "Game Over"
+print(message)
